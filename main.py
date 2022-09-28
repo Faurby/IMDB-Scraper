f568de3a5c464b281dfdebb4c295951772e4b95c
Faurby <lasse_fk@hotmail.com>
2022-09-28 22:21:41 +0200

    feat!: add a lot of stuff

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
-from flask import Flask, render_template
+from urllib import request
+from flask import Flask, render_template, redirect, url_for
+from flask_wtf import FlaskForm
+from wtforms import Form, StringField, validators, SubmitField
+import os
+import scrape
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(24).hex()
 
-@app.route("/")
-def home():
-    return render_template("home.html")
+
+class BasicForm(FlaskForm):
+    seriesID = StringField('SeriesID', validators=[validators.input_required()])
+    seasons = StringField('Seasons', validators=[validators.input_required()])
+    submit = SubmitField("Submit")
+
+@app.route("/", methods=['POST', 'GET'])
+def index():
+    form = BasicForm()
+    searchString = ""
+    seasons = 0
+    data = ""
+    if form.validate_on_submit():
+        searchString = form.seriesID._value()
+        seasons = form.seasons._value()
+        data = scrape.getSeasonAverage(searchString, seasons)
+
+    return render_template("index.html", form = form, data = data)
     
-@app.route("/about")
-def about():
-    return render_template("about.html")
-
 if __name__ == "__main__":
     app.run(debug=True)
--- scrape.py
+++ scrape.py
@@ -7,65 +7,84 @@ episodes = []
 
 rating_average = dict()
 
-seasons = 4
-
 # Series id
 seriesID = "tt4158110"
+
 query = "https://www.imdb.com/title/" + seriesID + "/episodes?season="
 
-# For every season in the series-- range depends on the show
-for sn in range(1, seasons+1):
-    # Request from the server the content of the web page by using get(), and store the server’s response in the variable response
-    response = get(query + str(sn))
-    # Parse the content of the request with BeautifulSoup
-    page_html = BeautifulSoup(response.text, 'html.parser')
+def getSeasonAverage(seriesID, seasons):
+    query = "https://www.imdb.com/title/" + seriesID + "/episodes?season="
+    for sn in range(1, int(seasons)+1):
+        response = get(query + str(sn))
+        page_html = BeautifulSoup(response.text, 'html.parser')
 
-    # Select all the episode containers from the season's page
-    episode_containers = page_html.find_all('div', class_ = 'info')
+        episode_containers = page_html.find_all('div', class_ = 'info')
 
-    episode_count = len(episode_containers)
-    rating_sum = 0
+        episode_count = len(episode_containers)
+        rating_sum = 0
 
-    # For each episode in each season
-    for episode in episode_containers:
-            # Get the info of each episode on the page
-            season = sn
-            episode_number = episode.meta['content']
-            title = episode.a['title']
-            airdate = episode.find('div', class_='airdate').text.strip()
+        for episode in episode_containers:
             rating = episode.find('span', class_='ipl-rating-star__rating').text
-
             rating_sum += float(rating)
 
-            total_votes = episode.find('span', class_='ipl-rating-star__total-votes').text
-            desc = episode.find('div', class_='item_description').text.strip()
-            # Compiling the episode info
-            episode_data = [season, episode_number, title, airdate, rating, total_votes, desc]
+        rating_average[sn] = round((rating_sum / episode_count), 1)
+    return rating_average
 
-            # Append the episode info to the complete dataset
-            episodes.append(episode_data)
-    rating_average[sn] = round((rating_sum / episode_count), 1)
+t = getSeasonAverage(seriesID, 4)
+
+# # For every season in the series-- range depends on the show
+# for sn in range(1, seasons+1):
+#     # Request from the server the content of the web page by using get(), and store the server’s response in the variable response
+#     response = get(query + str(sn))
+#     # Parse the content of the request with BeautifulSoup
+#     page_html = BeautifulSoup(response.text, 'html.parser')
+
+#     # Select all the episode containers from the season's page
+#     episode_containers = page_html.find_all('div', class_ = 'info')
+
+#     episode_count = len(episode_containers)
+#     rating_sum = 0
+
+#     # For each episode in each season
+#     for episode in episode_containers:
+#             # Get the info of each episode on the page
+#             season = sn
+#             episode_number = episode.meta['content']
+#             title = episode.a['title']
+#             airdate = episode.find('div', class_='airdate').text.strip()
+#             rating = episode.find('span', class_='ipl-rating-star__rating').text
+
+#             rating_sum += float(rating)
+
+#             total_votes = episode.find('span', class_='ipl-rating-star__total-votes').text
+#             desc = episode.find('div', class_='item_description').text.strip()
+#             # Compiling the episode info
+#             episode_data = [season, episode_number, title, airdate, rating, total_votes, desc]
+
+#             # Append the episode info to the complete dataset
+#             episodes.append(episode_data)
+#     rating_average[sn] = round((rating_sum / episode_count), 1)
     
 
-# Create panda dataframes
-episodes = pd.DataFrame(episodes, columns=['season', 'episode_number', 'title', 'airdate', 'rating', 'total_votes', 'desc'])
-rating_average = pd.DataFrame.from_dict(rating_average, orient='index')
+# # Create panda dataframes
+# episodes = pd.DataFrame(episodes, columns=['season', 'episode_number', 'title', 'airdate', 'rating', 'total_votes', 'desc'])
+# rating_average = pd.DataFrame.from_dict(rating_average, orient='index')
 
-# Remove , ( ) from votes
-def remove_str(votes):
-    for r in ((',',''), ('(',''),(')','')):
-        votes = votes.replace(*r)
+# # Remove , ( ) from votes
+# def remove_str(votes):
+#     for r in ((',',''), ('(',''),(')','')):
+#         votes = votes.replace(*r)
         
-    return votes
+#     return votes
 
-episodes['total_votes'] = episodes.total_votes.apply(remove_str).astype(int)
+# episodes['total_votes'] = episodes.total_votes.apply(remove_str).astype(int)
 
-# Parse rating as float
-episodes['rating'] = episodes.rating.astype(float)
+# # Parse rating as float
+# episodes['rating'] = episodes.rating.astype(float)
 
-# Parse airdate as datetime
-episodes['airdate'] = pd.to_datetime(episodes.airdate)
+# # Parse airdate as datetime
+# episodes['airdate'] = pd.to_datetime(episodes.airdate)
 
-# Save to csv
-episodes.to_csv('Episodes_IMDb_Ratings.csv', index=False)
-rating_average.to_csv('Average_Season_IMD_Ratings.csv', header=False)
+# # Save to csv
+# episodes.to_csv('Episodes_IMDb_Ratings.csv', index=False)
+# rating_average.to_csv('Average_Season_IMD_Ratings.csv', header=False)
